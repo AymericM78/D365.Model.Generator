bb352a789e0f599b49a4a2bbf53dd91cfdd543ce
Aymeric Mouillé <amouille@microsoft.com>
2018-12-06 11:36:32 +0100

    Upgrade packages + release 2018.12.06.1136

--- D365 Model Generator/Properties/AssemblyInfo.cs
+++ D365 Model Generator/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.03.05.1642")]
-[assembly: AssemblyFileVersion("2018.03.05.1642")]
+[assembly: AssemblyVersion("2018.12.06.1136")]
+[assembly: AssemblyFileVersion("2018.12.06.1136")]
